3b77089d49f841af9fd224a29d0425dcb39db4e4
Sergei Tsarik <serge.tsarik@gmail.com>
2025-02-01 22:59:56 +0300

    remove some classes

---
+++ src/main/java/com/herokuapp/the_internet/SomeClass.java
@@ -0,0 +1,4 @@
+package com.herokuapp.the_internet;
+
+public class SomeClass {
+}
--- src/main/java/com/herokuapp/the_internet/dynamic_controls/DynamicControlsPage.java
+++ src/main/java/com/herokuapp/the_internet/dynamic_controls/DynamicControlsPage.java
@@ -1,6 +1,7 @@
 package com.herokuapp.the_internet.dynamic_controls;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,8 +15,6 @@ public class DynamicControlsPage {
     private final int DEFAULT_DURATION_OF_SECONDS = 4;
 
     private final String CHECKBOX_EXAMPLE_BUTTON_LOCATOR = "//form[@id='checkbox-example']/button";
-    private final String INITIAL_CHECKBOX_INPUT_LOCATOR = "//div[@id='checkbox']/input";
-    private final String CHECKBOX_INPUT_LOCATOR = "//input[@id='checkbox']";
     private final String MESSAGE_LOCATOR = "//p[@id='message']";
 
     private WebDriver driver;
@@ -32,6 +31,17 @@ public class DynamicControlsPage {
     }
 
     public String getMessageText() {
+        /**
+         * Waiting Strategies
+         * https://www.selenium.dev/documentation/webdriver/waits/
+         * - Explicit waits
+         * WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_DURATION_OF_SECONDS));
+         * WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(MESSAGE_LOCATOR)));
+         */
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_DURATION_OF_SECONDS));
+        WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(MESSAGE_LOCATOR)));
+        return message.getText();
+
         /**
          * Waiting Strategies
          * https://www.selenium.dev/documentation/webdriver/waits/
@@ -56,6 +66,8 @@ public class DynamicControlsPage {
          *                .pollingEvery(Duration.ofMillis(500))
          *                .ignoring(NoSuchElementException.class);
          *
+         * WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(MESSAGE_LOCATOR)));
+         *
          * wait.until(d -> driver.findElement(By.xpath(MESSAGE_LOCATOR)).isDisplayed());
          */
         /*
@@ -66,8 +78,11 @@ public class DynamicControlsPage {
                         .ignoring(NoSuchElementException.class);
 
         wait.until(d -> driver.findElement(By.xpath(MESSAGE_LOCATOR)).isDisplayed());
+
+        WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(MESSAGE_LOCATOR)));
+        return message.getText();
         */
 
-        return driver.findElement(By.xpath(MESSAGE_LOCATOR)).getText();
+        // return driver.findElement(By.xpath(MESSAGE_LOCATOR)).getText();
     }
 }
